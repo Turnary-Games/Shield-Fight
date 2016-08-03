e9c4436741591201bc5359058e2be664d556d695
Bejno <bejnofalk@gmail.com>
2016-08-04 00:32:19 +0200

    forgot to add everything in last commit

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class CollisionDetection : MonoBehaviour {
 
+    public GameObject shieldParticlePrefab;
+
     void OnCollisionEnter(Collision other) {
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("Player Hit ");
+            GameObject clone = Instantiate(shieldParticlePrefab, transform.position, transform.rotation) as GameObject;
+            Destroy(clone, 2);
         }
     }
 }
